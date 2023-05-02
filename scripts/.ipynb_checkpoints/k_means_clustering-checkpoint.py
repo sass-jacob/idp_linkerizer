9ab33cf686d4cb5db4d9009e332e68ccbfafd18d
sass-jacob <sassj@mit.edu>
2023-05-02 18:28:11 -0400

    Update

--- scripts/.ipynb_checkpoints/k_means_clustering-checkpoint.py
+++ scripts/.ipynb_checkpoints/k_means_clustering-checkpoint.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 import sklearn
+from sklearn_extra.cluster import KMedoids
 
 def load_in_encoded_linkers():
     encoded_linkers = np.load('../saved_files/encoded_reduced_linkers.npz')['arr_0']
     return encoded_linkers
+
 
 def generate_elbow_plot_full_dim(encoded_linkers):
     #define the range of k values to test
@@ -26,52 +29,84 @@ def generate_elbow_plot_full_dim(encoded_linkers):
     plt.title('Elbow plot for KMeans clustering')
     plt.savefig('figures/kmeans_elbow.png')
 
+    
 def pick_cluster_representatives(encoded_linkers,k):
-    kmeans = KMeans(n_clusters=k, n_init="auto")
+    kmeans = KMeans(n_clusters=k, n_init="auto", max_iter=5000, random_state=42)
     kmeans.fit(encoded_linkers)
     print('fit kmeans')
     distances = kmeans.transform(encoded_linkers)
-    print(np.shape(distances))
-    closest_indices = np.array(np.argmin(distances, axis=1))
+    print('distances shape: {}'.format(np.shape(distances)))
+    closest_indices = np.array(np.argmin(distances, axis=0))
+    print('indices shape: {}'.format(np.shape(closest_indices))) 
     representatives = np.zeros((k, 48))
     #find the representatives
     for i in range(k):
-        cluster_indices = np.where(closest_indices == i)[0]
-        cluster_distances = distances[cluster_indices, i]
-        min_index = cluster_indices[np.argmin(cluster_distances)]
-        representatives[i,:] = encoded_linkers[min_index]
+        representatives[i,:] = encoded_linkers[closest_indices[i]]
     return representatives, closest_indices
+
+
+def pick_cluster_minibatched(encoded_linkers,k):
+    kmeans = MiniBatchKMeans(n_clusters=k, n_init="auto", max_iter=5000, random_state=42, batch_size=100000)
+    kmeans.fit(encoded_linkers)
+    print('fit kmeans mini batch')
+    distances = kmeans.transform(encoded_linkers)
+    print('distances shape: {}'.format(np.shape(distances)))
+    closest_indices = np.array(np.argmin(distances, axis=0))
+    print('indices shape: {}'.format(np.shape(closest_indices))) 
+    representatives = np.zeros((k, 48))
+    #find the representatives
+    for i in range(k):
+        representatives[i,:] = encoded_linkers[closest_indices[i]]
+    return representatives, closest_indices
+
 
 def save_nparray(nparray,filename):
     #save linker dataset as filename 
     np.savez_compressed(filename+'.npz', nparray)
     return
 
+
 import pandas as pd
 def decode(representatives, aa_df):
     rep_linkers = []
-    for unstd_rep in representatives:
+    for representative in representatives[1:]:
         sequence = ''
-        for encoding_index in range(len(unstd_rep)):
+        for encoding_index in range(len(representative)):
             if (encoding_index - 1) % 8 == 0:
-                sequence += aa_df.loc[aa_df['VHSE1'] == unstd_rep[encoding_index-1]].index
-        rep_linkers.append(sequence)
+                #print(aa_df.loc[aa_df['VHSE1'] == representative[encoding_index-1]].index)
+                sequence += (aa_df.loc[aa_df['VHSE1'] == representative[encoding_index-1]]).index
+        rep_linkers.append(sequence[0])
+    rep_linkers = rep_linkers[1:]
     return rep_linkers
+
+
+from sklearn_extra.cluster import KMedoids
+#too memory intensive because computing pairwise distances
+def pick_cluster_medoids(encoded_linkers,k):
+    kmedoids = KMedoids(n_clusters=k, metric='euclidean')
+    kmedoids.fit(encoded_linkers)
+    medoid_indices = kmedoids.medoid_indices_
+    representatives = encoded_linkers[medoid_indices]
+    return representatives, medoid_indices
+
 
 if __name__ == '__main__':
     encoded_linkers = load_in_encoded_linkers()
-    print(np.shape(encoded_linkers))
-    centroids, closest_indices = pick_cluster_representatives(encoded_linkers, 100)
-    #centroids = np.load('../saved_files/encoded_reps.npz')['arr_0']
-    #closest_indices = np.load('../saved_files/medoid_indices.npz')['arr_0']
-    print(np.shape(centroids))
-    print(np.shape(closest_indices))
-    save_nparray(centroids, '../saved_files/encoded_reps')
-    save_nparray(closest_indices, '../saved_files/medoid_indices')
-    amino_acid_df = pd.read_csv("../saved_files/amino_acid.csv")
-    amino_acid_df = amino_acid_df.set_index('Amino Acids')
-    print(np.shape(amino_acid_df))
-    representatives = decode(centroids, amino_acid_df)
-    print(representatives)
+    print(encoded_linkers[1000:1100])
+    #medoids, medoid_indices = pick_cluster_medoids(encoded_linkers, 100)
+    #centroids, closest_indices = pick_cluster_representatives(encoded_linkers, 100)
+    #centroids, closest_indices = pick_cluster_representatives(encoded_linkers, 100)
+    #centroids = np.load('../saved_files/encoded_near_centroid.npz')['arr_0']
+    #closest_indices = np.load('../saved_files/near_centroid_indices.npz')['arr_0']
+    #print(centroids)
+    #print(closest_indices)
+    #save_nparray(centroids, '../saved_files/encoded_near_centroid')
+    #save_nparray(closest_indices, '../saved_files/near_centroid_indices')
+    #amino_acid_df = pd.read_csv("../saved_files/amino_acid.csv")
+    #amino_acid_df = amino_acid_df.set_index('Amino Acids')
+    #print(np.shape(amino_acid_df))
+    #representatives = decode(centroids, amino_acid_df)
+    #print(representatives)
+    #print(np.shape(representatives))
     #representative_linkers = unstandardize_and_decode(centroids, dfs['all'])
     #save_nparray(representative_linkers, 'saved_files/decoded_reps')
--- scripts/reduced_linker_set.py
+++ scripts/reduced_linker_set.py
@@ -1,46 +1,27 @@
 import numpy as np
 import itertools
-
-print('beginning')
+import pandas as pd
 
 def generate_unencoded_linkers():
     #generate data set to perform clustering upon
-
-    #array of amino acids to consider
     #removed W due to low likelihood of linker functionality
     #removed C to prevent disulfide bonding in linker
     #removed P due to low likelihood of linker functionality
     amino_acids = np.array(['A', 'R', 'N', 'D', 'G', 'I', 'K', 'M', 'F', 'S', 'T', 'Y', 'V', 'Q', 'E', 'L', 'H'])
-
     #linker length we want to generate (can be more than a single value - began at 6)
     min_link_len = 6
     max_link_len = 6
-
     #initialize blank linker array
     linkers = []
-
-    #algorithm that generates all possible linkers of length 6 with given 'amino acid' array
+    #algorithm that generates all possible linkers of lengths min -> max with given 'amino acid' array
     for link_size in range(min_link_len, max_link_len+1):
         for item in itertools.product(amino_acids, repeat=link_size):
-            linkers.append(''.join(list(item)))
-        
+            linkers.append(''.join(list(item)))   
     return linkers
 
 def save_linkerfile(linkers,filename):
     #save linker dataset as filename 
     np.savez_compressed(filename+'.npz', linkers)
-
-#reduced_linkers = generate_unencoded_linkers()
-reduced_linkers = np.load('../saved_files/reduced_linkers.npz')['arr_0']
-print(np.shape(reduced_linkers))
-#save_linkerfile(reduced_linkers,'../saved_files/reduced_linkers')
-
-import pandas as pd
-amino_acid_df = pd.read_csv("../saved_files/amino_acid.csv")
-amino_acid_df = amino_acid_df.set_index('Amino Acids')
-featurized = np.zeros((len(reduced_linkers), 6 * len(np.array(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']])[0])))
-print(np.shape(featurized))
-amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
 
 def set_features(input_peptide):
     out_row = []
@@ -48,17 +29,30 @@ def set_features(input_peptide):
     for letter in peptide:
         which_amino_index = np.where(np.array(amino_acids) == letter)[0]
         # pick the row corresponding to the amino acid to be appended to the output
-        #print(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']].to_numpy()[which_amino_index])
-        feature = np.array(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']].to_numpy())[which_amino_index]
+        feature = np.array(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']].to_numpy()[which_amino_index])[0]
         out_row = np.append(out_row,feature)
     return out_row
 
-# featurize all of the data
-index = 0
-for linker in reduced_linkers:
-    featurized[index, :] = set_features(linker)
-    index += 1
-    if index % 10000 == 0:
-        print(index)
+def load_in_features():
+    amino_acid_df = pd.read_csv("../saved_files/amino_acid.csv")
+    amino_acid_df = amino_acid_df.set_index('Amino Acids')
+    return amino_acid_df
 
-#save_linkerfile(featurized,'../saved_files/encoded_reduced_linkers')
+if __name__ == '__main__':
+    #reduced_linkers = generate_unencoded_linkers()
+    reduced_linkers = np.load('../saved_files/reduced_linkers.npz')['arr_0']
+    amino_acid_df = load_in_features()
+    featurized = np.zeros((len(reduced_linkers), 6 * len(np.array(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']])[0])))    
+    amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+    # featurize all of the data
+    index = 0
+    for linker in reduced_linkers:
+        #print(set_features(linker))
+        featurized[index] = set_features(linker)
+        #print(np.shape(featurized))
+        if index % 100000 == 0:
+            print((index, featurized[index]))
+        index += 1
+
+    print(featurized[-10:])
+    #save_linkerfile(featurized,'../saved_files/encoded_reduced_linkers')
