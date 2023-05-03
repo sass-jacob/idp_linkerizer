1ef85c47acc4f1af9158d5abd017c877af6febd4
sass-jacob <sassj@mit.edu>
2023-05-02 21:42:32 -0400

    Please

--- scripts/.ipynb_checkpoints/k_means_clustering-checkpoint.py
+++ scripts/.ipynb_checkpoints/k_means_clustering-checkpoint.py
@@ -3,14 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
-import sklearn
 from sklearn_extra.cluster import KMedoids
+import sklearn
 
+#load in the encoded linkerset
 def load_in_encoded_linkers():
     encoded_linkers = np.load('../saved_files/encoded_reduced_linkers.npz')['arr_0']
     return encoded_linkers
 
-
+#elbow plot (unused)
 def generate_elbow_plot_full_dim(encoded_linkers):
     #define the range of k values to test
     k_range = range(1, 26)
@@ -29,7 +30,7 @@ def generate_elbow_plot_full_dim(encoded_linkers):
     plt.title('Elbow plot for KMeans clustering')
     plt.savefig('figures/kmeans_elbow.png')
 
-    
+#clustering using kmeans clustering
 def pick_cluster_representatives(encoded_linkers,k):
     kmeans = KMeans(n_clusters=k, n_init="auto", max_iter=5000, random_state=42)
     kmeans.fit(encoded_linkers)
@@ -44,7 +45,7 @@ def pick_cluster_representatives(encoded_linkers,k):
         representatives[i,:] = encoded_linkers[closest_indices[i]]
     return representatives, closest_indices
 
-
+#clustering using batched kmeans clustering
 def pick_cluster_minibatched(encoded_linkers,k):
     kmeans = MiniBatchKMeans(n_clusters=k, n_init="auto", max_iter=5000, random_state=42, batch_size=100000)
     kmeans.fit(encoded_linkers)
@@ -65,18 +66,15 @@ def save_nparray(nparray,filename):
     np.savez_compressed(filename+'.npz', nparray)
     return
 
-
-import pandas as pd
+#decode the encoded representatives
 def decode(representatives, aa_df):
     rep_linkers = []
-    for representative in representatives[1:]:
+    for representative in representatives:
         sequence = ''
         for encoding_index in range(len(representative)):
             if (encoding_index - 1) % 8 == 0:
-                #print(aa_df.loc[aa_df['VHSE1'] == representative[encoding_index-1]].index)
                 sequence += (aa_df.loc[aa_df['VHSE1'] == representative[encoding_index-1]]).index
         rep_linkers.append(sequence[0])
-    rep_linkers = rep_linkers[1:]
     return rep_linkers
 
 
@@ -92,21 +90,19 @@ def pick_cluster_medoids(encoded_linkers,k):
 
 if __name__ == '__main__':
     encoded_linkers = load_in_encoded_linkers()
-    print(encoded_linkers[1000:1100])
     #medoids, medoid_indices = pick_cluster_medoids(encoded_linkers, 100)
-    #centroids, closest_indices = pick_cluster_representatives(encoded_linkers, 100)
-    #centroids, closest_indices = pick_cluster_representatives(encoded_linkers, 100)
+    centroids, closest_indices = pick_cluster_representatives(encoded_linkers, 100)
     #centroids = np.load('../saved_files/encoded_near_centroid.npz')['arr_0']
     #closest_indices = np.load('../saved_files/near_centroid_indices.npz')['arr_0']
-    #print(centroids)
-    #print(closest_indices)
+    print(centroids)
+    print(closest_indices)
     #save_nparray(centroids, '../saved_files/encoded_near_centroid')
     #save_nparray(closest_indices, '../saved_files/near_centroid_indices')
-    #amino_acid_df = pd.read_csv("../saved_files/amino_acid.csv")
-    #amino_acid_df = amino_acid_df.set_index('Amino Acids')
-    #print(np.shape(amino_acid_df))
-    #representatives = decode(centroids, amino_acid_df)
-    #print(representatives)
-    #print(np.shape(representatives))
+    amino_acid_df = pd.read_csv("../saved_files/amino_acid.csv")
+    amino_acid_df = amino_acid_df.set_index('Amino Acids')
+    print(np.shape(amino_acid_df))
+    representatives = decode(centroids, amino_acid_df)
+    print(representatives)
+    print(np.shape(representatives))
     #representative_linkers = unstandardize_and_decode(centroids, dfs['all'])
-    #save_nparray(representative_linkers, 'saved_files/decoded_reps')
+    save_nparray(representatives, '../saved_files/decoded_representatives')
