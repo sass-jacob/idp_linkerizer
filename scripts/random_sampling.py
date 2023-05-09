f496ee543ce3976198fe85ff92099669ca8e49b6
Jacob Sass <80420670+sass-jacob@users.noreply.github.com>
2023-05-09 18:05:47 -0400

    Clearing out the old logs and updating the scripts

--- scripts/random_sampling.py
+++ scripts/random_sampling.py
@@ -5,9 +5,9 @@ def save_linkerfile(linkers,filename):
     #save linker dataset as filename 
     np.savez_compressed(filename+'.npz', linkers)
 
-linkers = np.load('../saved_files/reduced_linkers.npz')['arr_0']
-print(np.shape(linkers))
-random_linkers = linkers[random.sample(range(len(linkers)), 100)]
-print(random_linkers)
-
-#save_linkerfile(random_linkers, '../saved_files/second_100_random')
+if __name__ == '__main__':
+    linkers = np.load('../saved_files/reduced_linkers.npz')['arr_0']
+    print(np.shape(linkers))
+    random_linkers = linkers[random.sample(range(len(linkers)), 100)]
+    print(random_linkers)
+    #save_linkerfile(random_linkers, '../saved_files/insert_random_linkers_name_here')
--- scripts/reduced_linker_set.py
+++ scripts/reduced_linker_set.py
@@ -28,7 +28,7 @@ def set_features(input_peptide):
     peptide = [*input_peptide]
     for letter in peptide:
         which_amino_index = np.where(np.array(amino_acids) == letter)[0]
-        # pick the row corresponding to the amino acid to be appended to the output
+        #pick the row corresponding to the amino acid to be appended to the output
         feature = np.array(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']].to_numpy()[which_amino_index])[0]
         out_row = np.append(out_row,feature)
     return out_row
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     amino_acid_df = load_in_features()
     featurized = np.zeros((len(reduced_linkers), 6 * len(np.array(amino_acid_df[['VHSE1', 'VHSE2', 'VHSE3', 'VHSE4','VHSE5', 'VHSE6', 'VHSE7', 'VHSE8']])[0])))    
     amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
-    # featurize all of the data
+    #featurize all of the data
     index = 0
     for linker in reduced_linkers:
         #print(set_features(linker))
